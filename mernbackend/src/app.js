46e5e499930e8b6bfb79fb540c5b61fe860b0217
sakshijainn <sakshijjain1997@gmail.com>
2020-12-21 17:23:19 +0530

    update 3

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const hbs = require("hbs");
+const { response } = require("express");
 require("./db/conn");
+
+const Register = require("./models/registers");
 const port = process.env.PORT || 3000;
 
 
@@ -14,6 +17,9 @@ const template_path = path.join(__dirname ,  "../templates/views");
 const partials_path = path.join(__dirname ,  "../templates/partials/");
 // console.log(path.join(__dirname ,  "../templates/views"));
 
+
+app.use(express.json());
+app.use(express.urlencoded({extended:false}));
 
 app.use(express.static(static_path));
 app.set('view engine' , 'hbs');
@@ -27,6 +33,29 @@ app.get("/",(request,response)=>{
     response.render("index");
     // res.render("index");
 });
+app.get('/register', (request,response)=>
+{
+    response.render("register");
+})
+
+
+app.post('/register', async(request,response)=>
+{
+    try
+    {
+        console.log(request.body.firstname);
+        response.send(request.body.firstname);
+
+    }
+    catch (error)
+    {
+        response.status(400).send(error);
+    }
+})
+
+app.get("/login" ,(request,response)=>{
+    response.render("login");
+})
 
 
 app.listen(port, ()=>
---
+++ mernbackend/src/models/registers.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const employeeSchema = new mongoose.Schema({
+    firstname : {
+        type: String,
+        required:true
+    },
+    email :{
+        type: String,
+        required:true
+    },
+    password:
+    {
+        type: String,
+        required:true
+    },
+
+    gender:
+    {
+        type: String,
+        required:true
+    }, 
+    birthday :
+    {
+        type: String,
+        required:true
+    }
+
+})
+
+const Register =  new mongoose.model("Register", employeeSchema);
+
+module.exports= Register;
