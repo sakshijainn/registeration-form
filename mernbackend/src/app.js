4894fcc85dd86c8bfd77c7323d8a79432529ff02
sakshijainn <sakshijjain1997@gmail.com>
2020-12-21 12:18:13 +0530

    Update app.js

--- mernbackend/src/app.js
+++ mernbackend/src/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const hbs = require("hbs");
 require("./db/conn");
 const port = process.env.PORT || 3000;
 
@@ -10,16 +11,19 @@ const static_path = path.join(__dirname ,  "../public");
 // console.log(path.join(__dirname ,  "../public"));
 
 const template_path = path.join(__dirname ,  "../template/views");
+const partials_path = path.join(__dirname ,  "../template/partials");
+
+
 app.use(express.static(static_path));
-
 app.set("view engine" , "hbs");
-
 app.set("views", template_path);
+hbs.registerPartials(partials_path);
 
 
 app.get("/",(req,res)=>{
     // res.send("Welcome to Olaacademy Institution ");
-    res.render("index");
+    res.render("index")
+    // res.render("index");
 });
 
 
